4e8c74468d4279e602a801c5c9d6663ffdc2a0d4
thatssushmit <thatsushmit@gmail.com>
2022-02-18 02:39:29 +0530

    minor changes in jquary

--- script.js
+++ script.js
@@ -1,34 +1,13 @@
 $(document).ready(function(){
-    $(window).scroll(function(){
-        // sticky navbar on scroll script
-        if(this.scrollY > 20){
-            $('.navbar').addClass("sticky");
-        }else{
-            $('.navbar').removeClass("sticky");
-        }
-        
-        // scroll-up button show/hide script
-        if(this.scrollY > 500){
-            $('.scroll-up-btn').addClass("show");
-        }else{
-            $('.scroll-up-btn').removeClass("show");
-        }
-    });
-    // slide-up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
-        // removing smooth scroll on slide-up button click
-        $('html').css("scrollBehavior", "auto");
-    });
-    $('.navbar .menu li a').click(function(){
-        // applying again smooth scroll on menu items click
-        $('html').css("scrollBehavior", "smooth");
-    });
+    // $('.navbar .menu li a').click(function(){
+    //     // applying again smooth scroll on menu items click
+    //     $('html').css("scrollBehavior", "smooth");
+    // });
     // toggle menu/navbar script
-    $('.menu-btn').click(function(){
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-    });
+    // $('.menu-btn').click(function(){
+    //     $('.navbar .menu').toggleClass("active");
+    //     $('.menu-btn i').toggleClass("active");
+    // });
     // typing text animation script
     var typed = new Typed(".typing", {
         strings: ["Programmer", "Web-Designer","Freelancer"],
@@ -37,7 +16,7 @@ $(document).ready(function(){
         loop: true
     });
     var typed = new Typed(".typing-2", {
-        strings: ["Programmer","Web-Designer","Freelancer"],
+        strings: ["Programmer","Web-Designer","Freelancer","Web-Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
@@ -65,3 +44,63 @@ $(document).ready(function(){
         }
     });
 });
+
+//navbar function
+
+
+ // slide-up script
+scrollupbtn = document.getElementById("scroll-up-btn");
+navBarSelector = document.getElementById("navbar")
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if(this.scrollY > 50){
+        navBarSelector.classList.add("sticky")
+    }
+    else{
+        navBarSelector.classList.remove("sticky")
+    }
+    if (this.scrollY > 500) {
+        scrollupbtn.classList.add("show")
+        // scrollupbtn.addClass("show")
+
+    } else {
+        scrollupbtn.classList.remove("show")
+        // scrollupbtn.style.display = "none";
+    }
+  }
+  // When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+//menu button in nsav bar 
+
+// menuButtons = document.getElementsByClassName("menu-btn")
+// for (let button in menuButtons){
+//     button.
+//     // button.click(function(){
+//     //     document.getElementsByClassName("navbar menu").classList.toggleClass("active")
+//     //     document.getElementsByClassName("menu-btn i").classList.toggleClass("active")
+    
+//     // })
+
+// }
+
+//typing oon home page
+
+
+stringsArrays = ["Programmer","Web-Designer","Freelancer","Web-Developer"]
+typingHomeSelector = document.getElementsByClassName("typing")
+
+
+for(let txt in stringsArrays){
+
+    let text = document.createTextNode(txt)
+    typingHomeSelector.appendChild(text)
+
+}
+
+
