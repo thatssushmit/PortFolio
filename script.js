49071cd617cc1863a183bafc4f30940b11405e7e
thatssushmit <thatsushmit@gmail.com>
2022-02-17 20:19:08 +0530

    minor changes

--- script.js
+++ script.js
@@ -31,13 +31,13 @@ $(document).ready(function(){
     });
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Developer", "Web-Designer","Freelancer"],
+        strings: ["Programmer", "Web-Designer","Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
     var typed = new Typed(".typing-2", {
-        strings: ["Developer","Web-Designer","Freelancer"],
+        strings: ["Programmer","Web-Designer","Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
