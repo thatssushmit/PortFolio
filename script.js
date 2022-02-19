037f818331845d0c074ed869b70ed95c4807f99c
thatssushmit <thatsushmit@gmail.com>
2022-02-19 11:47:23 +0530

    minor bug fix

--- script.js
+++ script.js
@@ -1,53 +1,3 @@
-$(document).ready(function(){
-    // $('.navbar .menu li a').click(function(){
-    //     // applying again smooth scroll on menu items click
-    //     $('html').css("scrollBehavior", "smooth");
-    // });
-    // toggle menu/navbar script
-    // $('.menu-btn').click(function(){
-    //     $('.navbar .menu').toggleClass("active");
-    //     $('.menu-btn i').toggleClass("active");
-    // });
-    // typing text animation script
-    var typed = new Typed(".typing", {
-        strings: ["Programmer", "Web-Designer","Freelancer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
-    var typed = new Typed(".typing-2", {
-        strings: ["Programmer","Web-Designer","Freelancer","Web-Developer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
-    // owl carousel script
-    $('.carousel').owlCarousel({
-        margin: 20,
-        loop: true,
-        autoplay: true,
-        autoplayTimeOut: 2000,
-        autoplayHoverPause: true,
-        responsive: {
-            0:{
-                items: 1,
-                nav: false
-            },
-            600:{
-                items: 2,
-                nav: false
-            },
-            1000:{
-                items: 3,
-                nav: false
-            }
-        }
-    });
-});
-
-//navbar function
-
-
  // slide-up script
 scrollupbtn = document.getElementById("scroll-up-btn");
 navBarSelector = document.getElementById("navbar")
@@ -76,31 +26,75 @@ function topFunction() {
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
-//menu button in nsav bar 
 
-// menuButtons = document.getElementsByClassName("menu-btn")
-// for (let button in menuButtons){
-//     button.
-//     // button.click(function(){
-//     //     document.getElementsByClassName("navbar menu").classList.toggleClass("active")
-//     //     document.getElementsByClassName("menu-btn i").classList.toggleClass("active")
+// function showMenu(){
+//    let  menuSelectors = document.getElementById("menu")
+
+//     // document.getElementById("navbar").classList.T
+//     menuSelectors.classList.toggle("active")
+//     document.getElementById("menu-btn1").childNodes[1].classList.toggle("active")
+//     document.getElementById("navbar").classList.toggle("active")
+//     let itemMenu = menuSelectors.children
+//     for(let menus in itemMenu){
+//         let menu = itemMenu[menus].children.
     
-//     // })
+
+//     }
+
+
+    
+
+    // console.log(menuButton)
+    // document.getElementsByClassName("navbar .menu").classList.toggle("active")
+    // document.getElementsByClassName(".menu-btn i").classList.toggle("active")
 
 // }
 
-//typing oon home page
+// a.addEventListener('click', e => {
+//     debugger
+//     e.currentTarget; // always returns "a" element
+//     e.target; // may return "a" or "span"
+// })
+
+
+
+
 
 
 stringsArrays = ["Programmer","Web-Designer","Freelancer","Web-Developer"]
 typingHomeSelector = document.getElementsByClassName("typing")
 
 
-for(let txt in stringsArrays){
+window.onload = function() {
+    var typed = new Typed(".typing", {
+        strings: stringsArrays,
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    var typed = new Typed(".typing-2", {
+        strings: stringsArrays,
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
 
-    let text = document.createTextNode(txt)
-    typingHomeSelector.appendChild(text)
+ 
 
 }
 
 
+$(document).ready(function(){
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+    // typing text animation script
+
+
+});
