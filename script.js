e03fe9b7fdabb93c43418b927381f35875bb27e8
thatssushmit <thatsushmit@gmail.com>
2022-02-28 16:55:08 +0530

    remvoed unwanted code

--- script.js
+++ script.js
@@ -97,9 +97,3 @@ $(document).ready(function(){
 
 });
 
-function sendEmail(message){
-    var email = message.emailId;
-    var subject = message.subject;
-    var emailBody = 'Hi '+message.from;
-    document.location = "mailto:"+email+"?subject="+subject+"&body="+emailBody;
-}
